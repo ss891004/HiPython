68d7063aeff4f8a225e2cc82d0502fa5241f7233
ss891004 <575726653@qq.com>
2022-03-07 10:27:42 +0800

    update

--- hi_flask/flask06/apps/user/view.py
+++ hi_flask/flask06/apps/user/view.py
@@ -8,8 +8,18 @@ user_bp = Blueprint('user', __name__,template_folder='templates')
 
 @user_bp.route('/')
 def index():
-    return 'hello ~~~'
+
+    # 查询当前所有的用户
+    users=User.query.all()
+
+    return render_template('index.html', us=users)
 
 @user_bp.route('/hi')
 def index2():
      return render_template('index.html', msg="这天气适合睡觉")
+
+
+# 注册
+@user_bp.route('/register')
+def index3():
+    pass
