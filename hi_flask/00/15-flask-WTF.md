0b6d61c54f4a5ab4eef6f326a41ddcd54daec1eb
ss891004 <575726653@qq.com>
2022-03-28 23:24:33 +0800

    update

---
+++ hi_flask/00/15-flask-WTF.md
@@ -0,0 +1,17 @@
+## flask-WTF
+
+https://flask-wtf.readthedocs.io/en/1.0.x/
+https://wtforms.readthedocs.io/en/3.0.x/
+
+
+## CSRF
+
+
+## 验证正则
+    + 11位手机号码
+
+## 文件上传
+
+## Cookie
+
+## session
--- hi_flask/flask07/exts/ext.py
+++ hi_flask/flask07/exts/ext.py
@@ -1,10 +1,14 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap5
 
 dbs = SQLAlchemy()
 migrate = Migrate()
+bootstrap = Bootstrap5()
+
 
 
 def init_ext(app):
     dbs.init_app(app)
     migrate.init_app(app, dbs)
+    bootstrap.init_app(app)
